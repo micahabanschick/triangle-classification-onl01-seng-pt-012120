0eccbf1afa2940f2399310eaa587ec16f92601b7
Micah Banschick <micha.ban@gmail.com>
2020-02-09 18:10:51 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,20 @@
 class Triangle
-  # write code here
+  # triangle code
+  attr_accessor :ab, :bc, :ca, :type
+  
+  def initialize(ab:,bc:,ca:)
+    @ab = ab 
+    @bc = bc 
+    @ca = ca 
+  end 
+  
+  def kind 
+    if @ab == 0 || @bc == 0 || @ca == 0 || (@ab + @bc) <= @ca ||  (@ab + @ca) <= @bc || (@ca + @bc) <= @ab
+      begin 
+        raise StandardError
+      rescue StandardError 
+ 
+  class TriangleError < StandardError
+    # triangle error code
+  end
 end
