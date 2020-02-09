3d6091cc13216412dd276b04295425bacb3978b0
Micah Banschick <micha.ban@gmail.com>
2020-02-09 20:03:23 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,36 @@
 class Triangle
-  # write code here
+  # triangle code
+  attr_accessor :ab, :bc, :ca, :type
+  
+  def initialize(ab,bc,ca)
+    @ab = ab
+    @bc = bc
+    @ca = ca
+  end 
+  
+  def kind 
+    if @ab <= 0 || @bc <= 0 || @ca <= 0 || (@ab + @bc) <= @ca ||  (@ab + @ca) <= @bc || (@ca + @bc) <= @ab
+      raise TriangleError
+=begin
+      begin 
+        raise Triangle::TriangleError.new()
+      rescue Triangle::TriangleError => error 
+        puts error.message
+      end
+=end 
+    elsif @ab == @bc && @bc == @ca 
+      @type = :equilateral 
+    elsif @ab == @bc || @bc == @ca || @ab == @ca 
+      @type = :isosceles 
+    else 
+      @type = :scalene 
+    end 
+  end 
+ 
+  class TriangleError < StandardError
+    # triangle error code
+    def message
+      "oops"
+    end 
+  end
 end
